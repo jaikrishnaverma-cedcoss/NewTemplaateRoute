d8383d5a801d30d1687c6c82ab65d38981a0958d
jaikrishnaverma <jaikrishnaverma>
2022-09-09 17:20:50 +0530

    Task2

--- src/component/Gallery.jsx
+++ src/component/Gallery.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
 function Gallery() {
-    return ( 
+    return (
         <>
-        <h1>Gallery Container</h1>
+           <img className="banner" src="board.jpg"></img>
+            <div className=" foot">
+                <h2 style={{ textAlign: "left" }}>How Much You Can Make</h2>
+                <div className="row">
+                    <input type="text" placeholder='Hours' />
+                    <input placeholder='city' type="text" />
+                    <button className="btn btn-warninggit ">CALCULATE</button>
+                </div>
+            </div>
         </>
-     );
+    );
 }
 
 export default Gallery;
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 function Home() {
     return ( 
         <>
-        <h1>Home Container</h1>
+        <img className="banner" src="clg.jpg" ></img>
+        <div className="foot">
+        <h2 style={{textAlign:"left"}}>How Much You Can Make</h2>
+        <div className="row">
+            <input type="text" placeholder='Hours' />
+            <input placeholder='city' type="text" />
+            <button className="btn btn-danger">CALCULATE</button>
+        </div>
+        </div>
         </>
      );
 }
